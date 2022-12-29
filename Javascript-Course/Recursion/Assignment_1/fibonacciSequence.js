9bfb6cd0bcb54c60fe895ae74736993ee9472bbf
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2022-12-29 18:55:07 +0530

    Recursion - Assignment_1

---
+++ Javascript-Course/Recursion/Assignment_1/fibonacciSequence.js
@@ -0,0 +1,28 @@
+// Iterative Way
+
+let n = 8;
+const fibo = function(n){
+    let arr= [1, 1];
+    for (let i = 3; i < n; i += 1) {
+      arr.push(arr[i - 2] + arr[i - 3]);
+    }
+    return arr; 
+}
+
+// N= 8 
+// Output = [1, 1, 2, 3, 5, 8, 13]
+
+// ***********************************************************************************************************************
+
+// Recusive
+const fiboRec = function(n, fibonacciArray = [0, 1]) {
+    if (fibonacciArray.length >= n) return fibonacciArray.slice(0, n + 1);
+  
+    const nextFibonacciNumber = fibonacciArray.at(-1) + fibonacciArray.at(-2);
+    fibonacciArray.push(nextFibonacciNumber);
+    return fiboRec(n, fibonacciArray);
+}
+
+
+// N= 8 
+// Output = [1, 1, 2, 3, 5, 8, 13]
